fc87f5a81f1fdf8bc3ebdf867b1095b267a5915f
craciun-st <67415094+craciun-st@users.noreply.github.com>
2020-12-08 14:40:14 +0200

    Add create board (based on title only) inside Python
    Function get_new_id_for_boards() takes care of getting the other missing
    field (the 'id') besides the 'title' in order to have a proper CSV
    entry.

--- data_handler.py
+++ data_handler.py
@@ -28,3 +28,31 @@ def get_cards_for_board(board_id):
             card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
             matching_cards.append(card)
     return matching_cards
+
+
+def get_new_id_for_boards():
+    """
+    Gets a new (valid) id for a board
+    :return: int representing the next available ID
+    """
+    all_boards = get_boards()
+    id_list = []
+    for board in all_boards:
+        try:
+           new_id = int(board['id'])
+        except (ValueError, TypeError):
+            pass
+        else:
+            id_list.append(new_id)
+
+    max_id = max(id_list)
+    return max_id + 1
+
+def create_new_board(title):
+    row_dict = {'id': get_new_id_for_boards(), 'title': title}
+    persistence.append_boards(row_dict)
+
+
+if __name__ == "__main__":
+    print(get_new_id_for_boards())
+    create_new_board('test')
