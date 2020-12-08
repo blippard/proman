4d34cb3fd99a7f2f70e4458b2758fe4e64d3e866
Solya Gergo <solya.gergo.sandor@gmail.com>
2020-12-08 12:07:36 +0100

    Added getCardsByBoardId in data_handler.js and loadCard, showCard in dom.js

--- static/js/dom.js
+++ static/js/dom.js
@@ -9,6 +9,9 @@ export let dom = {
         // retrieves boards and makes showBoards called
         dataHandler.getBoards(function(boards){
             dom.showBoards(boards);
+            for (let board of boards) {
+                dom.loadCards(board.id);
+            }
         });
     },
     showBoards: function (boards) {
@@ -19,21 +22,22 @@ export let dom = {
 
         for(let board of boards){
             boardList += `
-                <div data-toggle="collapse" data-target="#testCollapse">
-                    fill
-                    <div class="collapse" id="testCollapse">
-                        <div class="card card-body">
-                            Test card
-                        </div>
+                <section class="board">
+                    <div class="board-header">
+                        <span class="board-title">${board.title}</span>
+                        <button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#board${board.id}" aria-expanded="false" aria-controls="board${board.id}"></button>
                     </div>
-                </div>
+                    <div class="row collapse" id="board${board.id}">
+                        
+                    </div>
+                </section>
             `;
         }
 
         const outerHtml = `
-            <section class="board-container">
+            <div class="board-container">
                 ${boardList}
-            </section>
+            </div>
         `;
 
         let boardsContainer = document.querySelector('#boards');
@@ -41,10 +45,23 @@ export let dom = {
     },
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
+        dataHandler.getCardsByBoardId(boardId,function(cards){
+            dom.showCards(cards);
+        })
     },
     showCards: function (cards) {
         // shows the cards of a board
         // it adds necessary event listeners also
+        for (let card of cards){
+            let board = document.querySelector(`#board${card.board_id}`);
+            if (`board${card.board_id}` == `${board.id}`) {
+                if (!board.querySelector(`.${card.status_id}`)) {
+                    let cardColumn = document.createElement('div');
+                    cardColumn.setAttribute('class', `card-column ${card.status_id}`);
+                    board.appendChild(cardColumn);
+                }
+            }
+        }
     },
     // here comes more features
 };
