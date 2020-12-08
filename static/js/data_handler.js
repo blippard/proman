67058964e618eb8e2f2f619c3874882e0f00c7df
blippard <blippard@gmail.com>
2020-12-08 14:15:27 +0100

    Edit: Changed to _api_get method to retrieve JSON for cards

--- static/js/data_handler.js
+++ static/js/data_handler.js
@@ -43,7 +43,10 @@ export let dataHandler = {
     },
     getCardsByBoardId: function (boardId, callback) {
         // the cards are retrieved and then the callback function is called with the cards
-        fetch(`/get-cards/${boardId}`).then((response) => response.json()).then((data => console.log(data)));
+        this._api_get(`/get-cards/${boardId}`, (response) => {
+            this._data['cards'] = response;
+            callback(response);
+        });
     },
     getCard: function (cardId, callback) {
         // the card is retrieved and then the callback function is called with the card
