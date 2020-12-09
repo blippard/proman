e87974f81f54d7cb1019e5816d30ce401d5ff6d8
blippard <blippard@gmail.com>
2020-12-09 15:12:43 +0100

    Merge: Brought in funciton to add new board, enhanced new card with status and title input by user

--- static/js/data_handler.js
+++ static/js/data_handler.js
@@ -17,9 +17,19 @@ export let dataHandler = {
         .then(json_response => callback(json_response));  // Call the `callback` with the returned object
     },
     _api_post: function (url, data, callback) {
-        // it is not called from outside
-        // sends the data to the API, and calls callback function
-    },
+    // it is not called from outside
+    // sends the data to the API, and calls callback function
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json()) // parse the response as JSON
+      .then((json_response) => callback(json_response)); // Call the `callback` with the returned object
+  },
     init: function () {
     },
     getBoards: function (callback) {
@@ -55,10 +65,22 @@ export let dataHandler = {
         // the card is retrieved and then the callback function is called with the card
     },
     createNewBoard: function (boardTitle, callback) {
-        // creates new board, saves it and calls the callback function with its data
-    },
+        // creates new board, makes a request to save it and calls the callback function with its data
+        let dataToPost = { title: boardTitle };
+        this._api_post("/add-board", dataToPost, (jsonResponse) => {
+          if (jsonResponse.title) {
+            this._data["boards"].push(jsonResponse);
+          }
+          callback(jsonResponse);
+        }); //callback will act on response.json(),
+        //                                                  // not on response
+      },
     createNewCard: function (cardTitle, boardId, statusId, callback) {
         // creates new card, saves it and calls the callback function with its data
+        let newCardPostData = {'title': cardTitle, 'board_id': boardId, 'status_id':statusId}
+        this._api_post('/add-card', newCardPostData, (jsonResponse) => {
+        this._data["cards"].push(jsonResponse);
+        })
     },
     // here comes more features
     camelize: function (str) {
