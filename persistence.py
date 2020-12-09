db6c2adad441b7f9496b57f6f5d1f01b95545c32
blippard <blippard@gmail.com>
2020-12-09 15:11:46 +0100

    Merge: Brought in function to add new boards

--- persistence.py
+++ persistence.py
@@ -4,6 +4,10 @@ STATUSES_FILE = './data/statuses.csv'
 BOARDS_FILE = './data/boards.csv'
 CARDS_FILE = './data/cards.csv'
 
+STATUSES_HEADER = ['id', 'title']
+BOARDS_HEADER = ['id', 'title', 'board_statuses']
+CARDS_HEADER = ['id', 'board_id', 'title', 'status_id', 'order']
+
 _cache = {}  # We store cached data in this dict to avoid multiple file readings
 
 
@@ -34,6 +38,33 @@ def _get_data(data_type, file, force):
     return _cache[data_type]
 
 
+def _append_csv(file_name, data_type, row):
+    """
+    Appends the content of a .csv file with a single row (a dict)
+    :param file_name: relative path to file
+    :param data_type: a key ('boards', 'statuses' etc.) to help determine the
+      header for the .csv (the canonical order of keys)
+    :param row: a SINGLE dictionary with keys the sames as header for data_type
+    """
+    with open(file_name, 'a', newline='') as csvfile:
+        if data_type == 'boards':
+            my_fieldnames = BOARDS_HEADER
+        elif data_type == 'cards':
+            my_fieldnames = CARDS_HEADER
+        elif data_type == 'statuses':
+            my_fieldnames = STATUSES_HEADER
+        else:
+            print('Wrong data type!')
+            return
+
+        # turn the dictionary into a sorted list of values corresponding to the canonical
+        # ordering of the dictionary keys [this order is given by the header (my_fieldnames)]
+        values_list = [row[field] if field in row else "" for field in my_fieldnames]
+
+        writer = csv.writer(csvfile)
+        writer.writerow(values_list)
+
+
 def clear_cache():
     for k in list(_cache.keys()):
         _cache.pop(k)
@@ -49,3 +80,11 @@ def get_boards(force=False):
 
 def get_cards(force=False):
     return _get_data('cards', CARDS_FILE, force)
+
+
+def append_cards(board_dict):
+    _append_csv(CARDS_FILE, 'cards', board_dict)
+
+
+def append_boards(board_dict):
+    _append_csv(BOARDS_FILE, 'boards', board_dict)
