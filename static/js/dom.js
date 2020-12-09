a8fe28d29b2a1d76dd7fc51dda783f581a36e46d
blippard <blippard@gmail.com>
2020-12-09 15:13:20 +0100

    Merge: Brought in functions to add new board, corrected new card to fit

--- static/js/dom.js
+++ static/js/dom.js
@@ -4,6 +4,10 @@ import { dataHandler } from "./data_handler.js";
 export let dom = {
     init: function () {
         // This function should run once, when the page is loaded.
+        const newBoardBtn = document.querySelector(".add-board-btn");
+        newBoardBtn.addEventListener("click", (event) => {
+            this.handleAddBoardClick(event);
+        });
     },
     loadBoards: function () {
         // retrieves boards and makes showBoards called
@@ -20,12 +24,13 @@ export let dom = {
 
         let boardList = '';
 
-        for(let board of boards){
+        for (let board of boards) {
             boardList += `
                 <section class="board col mb-5 border border-dark">
                     <div class="board-header">
                         <span class="board-title">${board.title}</span>
                         <button class="btn btn-dark float-right" type="button" data-toggle="collapse" data-target="#board${board.id}" aria-expanded="false" aria-controls="board${board.id}"></button>
+                        <button class="new-card-btn" board-id="${board.id}">New Card</button>
                     </div>
                     <div class="row collapse" id="board${board.id}">
                         
@@ -42,6 +47,38 @@ export let dom = {
 
         let boardsContainer = document.querySelector('#boards');
         boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
+
+        this.addCardEventListener();
+        },
+    addCardEventListener: function ()  {
+        let newCardBtns = document.querySelectorAll('.new-card-btn');
+
+        for (let cardBtn of newCardBtns) {
+            let boardId = cardBtn.getAttribute('board-id');
+            cardBtn.addEventListener('click', function () {
+                // let cardTitle = prompt("Please add a title");
+                let cardForm = `
+                <form>
+                <input type="text" name="title">
+                <select name="status">
+                    <option value="0">New</option>
+                    <option value="1">In Progress</option>
+                    <option value="2">Testing</option>
+                    <option value="3">Done</option>
+                </select>
+                <input type="submit" id="new-card-submit">
+                </form>
+                `
+                cardBtn.insertAdjacentHTML("afterend", cardForm);
+
+                let form = document.querySelector('form')
+                    form.addEventListener('submit', event => {
+                        const formData = new FormData(event.target)
+                        dataHandler.createNewCard(formData.get('title'), boardId, formData.get('status'));
+                    })
+            })
+        }
+
     },
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
@@ -74,5 +111,21 @@ export let dom = {
             }
         }
     },
+    handleAddBoardClick: function (clickEvent) {
+    clickEvent.preventDefault();
+    let inputTitle = prompt("What is the title of the new board?");
+    if (!(inputTitle === "") && inputTitle) {
+      dataHandler.createNewBoard(inputTitle, (jsonResponse) => {
+        if (jsonResponse.title && jsonResponse.id) {
+          this.createNewChildBoard(jsonResponse);
+        } else {
+          window.alert(
+            "Could not get reply from server. Please wait and try again later."
+          );
+          // could maybe add a child that lives only until next _data update (next showBoards call)
+        }
+      });
+    }
+  },
     // here comes more features
 };
