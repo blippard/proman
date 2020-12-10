ba43e161bc4754c67f4cde26819902705f012d58
craciun-st <67415094+craciun-st@users.noreply.github.com>
2020-12-10 12:29:39 +0200

    Update status_response decorator with default 200 response

--- util.py
+++ util.py
@@ -18,29 +18,31 @@ def json_response(func):
 
 def status_response(func):
     """
-    Wraps a function that returns a dict of dicts into a Flask JSON response with status
+    Wraps a function (function which returns a dict of dicts) into a Flask response 
+    of type JSON with provided status ("status" key in dict of dicts)
+    
     :param func: function that should return at least {"json_data": dict_to_be_JSONed}
-        Hopefully it also contains a "status" key(e.g. {"json_data: ..., "status": ...})
+        Hopefully it also contains a "status" key (e.g. {"json_data": ..., "status": ...})
     :return:
     """
     @wraps(func)
     def decorated_function(*args, **kwargs):
         returned_value = func(*args, **kwargs)
         if "json_data" in returned_value:
-            payload = json.dumps(returned_value["json_data"])            
+            actual_data = json.dumps(returned_value["json_data"])            
         else:
             print("Function for the status response does not return a proper value!")
             raise ValueError
         if "status" in returned_value:            
             return Response(
-                payload, 
+                actual_data, 
                 status=returned_value["status"], 
                 mimetype='application/json'
             )
         else:
             return Response(
-                payload, 
-                status=returned_value["status"], 
+                actual_data, 
+                status=200, 
                 mimetype='application/json'
             )
 
