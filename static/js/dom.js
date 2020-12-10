2f6222fc25ecb1878d5f87741f331787c7156196
Solya Gergo <solya.gergo.sandor@gmail.com>
2020-12-10 09:34:21 +0100

    Added blank modal

--- static/js/dom.js
+++ static/js/dom.js
@@ -34,7 +34,7 @@ export let dom = {
                 <section class="board col mb-5 border border-dark" id="wholeBoard${board.id}">
                     <div class="board-header">
                         <span class="board-title">${board.title}</span>
-                        <button class="btn btn-dark add-column">Add column</button>
+                        <button class="btn btn-dark add-column" data-toggle="modal" data-target="#submitModal" data-board-id="${board.id}">Add column</button>
                         <button class="btn btn-dark float-right" type="button" data-toggle="collapse" data-target="#board${board.id}" aria-expanded="false" aria-controls="board${board.id}"></button>
                     </div>
                     <div class="row collapse" id="board${board.id}">
@@ -89,7 +89,7 @@ export let dom = {
     initAddColumn: function (event) {
         let boardSection = event.target.parentNode.parentNode
         if (boardSection.tagName === 'SECTION' && boardSection.classList.contains('board')) {
-            console.log(event.target.parentNode.parentNode);
+            console.log(event.target.dataset.boardId);
         }
     }
 };
