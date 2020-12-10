8425b43d26f4ee5b642b51a4e1e36acc8e9a4273
craciun-st <67415094+craciun-st@users.noreply.github.com>
2020-12-10 11:42:24 +0200

    Add new wrapper to enable to respond with HTTP status codes

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 from functools import wraps
-from flask import jsonify
+from flask import jsonify, Response, json
+
 
 
 def json_response(func):
@@ -11,5 +12,36 @@ def json_response(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         return jsonify(func(*args, **kwargs))
+
+    return decorated_function
+
+
+def status_response(func):
+    """
+    Wraps a function that returns a dict of dicts into a Flask JSON response with status
+    :param func: function that should return at least {"json_data": dict_to_be_JSONed}
+        Hopefully it also contains a "status" key(e.g. {"json_data: ..., "status": ...})
+    :return:
+    """
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        returned_value = func(*args, **kwargs)
+        if "json_data" in returned_value:
+            payload = json.dumps(returned_value["json_data"])            
+        else:
+            print("Function for the status response does not return a proper value!")
+            raise ValueError
+        if "status" in returned_value:            
+            return Response(
+                payload, 
+                status=returned_value["status"], 
+                mimetype='application/json'
+            )
+        else:
+            return Response(
+                payload, 
+                status=returned_value["status"], 
+                mimetype='application/json'
+            )
 
     return decorated_function
