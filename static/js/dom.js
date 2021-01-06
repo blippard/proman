17d44596d1cd2f3f0d1072c59dc0122953de49cd
fabiankata <fabian.kata97@gmail.com>
2021-01-06 09:42:05 +0100

    Commit for merging

--- static/js/dom.js
+++ static/js/dom.js
@@ -10,6 +10,7 @@ export let dom = {
         let addPrivateBoardButton = document.querySelector('.add-private-board-btn')
         let registerSubmit = document.getElementById('reg-button');
         let loginSubmit = document.getElementById('login-submit-button');
+        let privateBoardSubmit = document.getElementById('private-board-submit-button')
         let boards = document.getElementById('boards');
         let registrationContainer = document.getElementById('registration-container');
         let addPrivateBoardContainer = document.querySelector('.private-board-container')
@@ -42,6 +43,9 @@ export let dom = {
             registrationContainer.style.display = 'none';
             loginContainer.style.display = 'none';
             boards.style.display = 'none';
+        })
+        privateBoardSubmit.addEventListener('click', () => {
+
         })
         registerSubmit.addEventListener('click', () => {
             let username = document.getElementById('username');
@@ -98,7 +102,7 @@ export let dom = {
     loadBoards: function () {
         // retrieves boards and makes showBoards called
         this.removeAllBoardElements();
-        dataHandler.getBoards(function(boards){
+        dataHandler.getBoards(function (boards) {
             dom.showBoards(boards);
             for (let board of boards) {
                 dom.loadCards(board.id);
@@ -109,11 +113,11 @@ export let dom = {
         // shows boards appending them to #boards div
         // it adds necessary event listeners also
 
-    let boardList = "";
+        let boardList = "";
 
-        for(let board of boards){
+        for (let board of boards) {
             let columnlist = '';
-            for (let column of board.board_statuses){
+            for (let column of board.board_statuses) {
                 columnlist += `
                 <div class="col border border-dark p-0 ${dataHandler.camelize(column)}${board.id}">
                     <div class="card-column-title text-center border-bottom border-dark mb-2">${column}</div>
@@ -157,15 +161,15 @@ export let dom = {
 
         this.addCardEventListener();
         document.querySelectorAll(".remove-board-btn").forEach((item) => {
-          item.addEventListener("click", (event) => {
-            // the .board is the grandparent of the .remove-board-btn (the parent is .board-header)
-            this.handleRemoveBoardClick(item.parentNode.parentNode, event);
-          });
+            item.addEventListener("click", (event) => {
+                // the .board is the grandparent of the .remove-board-btn (the parent is .board-header)
+                this.handleRemoveBoardClick(item.parentNode.parentNode, event);
+            });
         });
         this.newNameBoardEventListener();
         this.initColumnTitleRename();
     },
-    addCardEventListener: function ()  {
+    addCardEventListener: function () {
         let newCardBtns = document.querySelectorAll('.new-card-btn');
 
         for (let cardBtn of newCardBtns) {
@@ -252,31 +256,31 @@ export let dom = {
         this.createDropZone();
     },
     handleAddBoardClick: function (clickEvent) {
-      clickEvent.preventDefault();
-      let inputTitle = prompt("What is the title of the new board?");
-      if (!(inputTitle === "") && inputTitle) {
-        dataHandler.createNewBoard(inputTitle, (jsonResponse) => {
-          if (jsonResponse.title && jsonResponse.id) {
-            this.createNewChildBoard(jsonResponse);
-          } else {
-            window.alert(
-              "Could not get reply from server. Please wait and try again later."
-            );
-            // could maybe add a child that lives only until next _data update (next showBoards call)
-          }
-        });
-      }
+        clickEvent.preventDefault();
+        let inputTitle = prompt("What is the title of the new board?");
+        if (!(inputTitle === "") && inputTitle) {
+            dataHandler.createNewBoard(inputTitle, (jsonResponse) => {
+                if (jsonResponse.title && jsonResponse.id) {
+                    this.createNewChildBoard(jsonResponse);
+                } else {
+                    window.alert(
+                        "Could not get reply from server. Please wait and try again later."
+                    );
+                    // could maybe add a child that lives only until next _data update (next showBoards call)
+                }
+            });
+        }
     },
     createNewChildBoard: function (board) {
         const boardInnerContainer = document.querySelector(".board-container");
         let columnlist = '';
-            for (let column of board.board_statuses){
-                columnlist += `
+        for (let column of board.board_statuses) {
+            columnlist += `
                 <div class="col border border-dark p-0 ${dataHandler.camelize(column)}${board.id}">
                     <div class="card-column-title text-center border-bottom border-dark mb-2">${column}</div>
                 </div>
                 `
-            }
+        }
         let childHTMLText = `
             <section class="board col mb-5 border border-dark" id="wholeBoard${board.id}" data-id="${board.id}">
                 <div class="board-header">
@@ -308,7 +312,7 @@ export let dom = {
 
         let newRemoveBoardBtn = boardInnerContainer.lastElementChild.querySelector(
             ".remove-board-btn"
-        );   
+        );
         newRemoveBoardBtn.addEventListener("click", (event) => {
             this.handleRemoveBoardClick(
                 newRemoveBoardBtn.parentNode.parentNode,
@@ -320,14 +324,14 @@ export let dom = {
     handleRemoveBoardClick: function (boardNode, clickEvent) {
         // boardNode is the node in the DOM tree corresponding to a board HTML element
         // and, thus, element and node are interchangeable for almost all purposes
-        clickEvent.preventDefault();        
+        clickEvent.preventDefault();
         let boardId = boardNode.dataset.id
         dataHandler.deleteBoard(boardId, (jsonResponse) => {
-        if (!(jsonResponse.id)) {
-            window.alert(
-            "Could not get reply from server. Will delete only temporarily!"
-            );
-        }
+            if (!(jsonResponse.id)) {
+                window.alert(
+                    "Could not get reply from server. Will delete only temporarily!"
+                );
+            }
         });
         // window.alert(`Deleted board with id=${boardNode.dataset.id}`)
         boardNode.remove(); // this will remove all children of the node as well
@@ -343,23 +347,24 @@ export let dom = {
         `
         boardBtnElement.insertAdjacentHTML("afterend", cardForm);
         let form = document.querySelector('form')
-            form.addEventListener('submit', event => {
-                const formData = new FormData(event.target)
-                dataHandler.renameBoard(boardId, formData.get('title'));
-            })
+        form.addEventListener('submit', event => {
+            const formData = new FormData(event.target)
+            dataHandler.renameBoard(boardId, formData.get('title'));
+        })
     },
     createDropZone: function () {
         let dropZone = document.querySelectorAll('.col.border.border-dark.p-0');
         let cards = document.querySelectorAll('div[card="true"]');
-            for (let card of cards) {
-                card.addEventListener('dragstart', event => {
-                    event.dataTransfer.setData("text/plain", card.id);
-                });
-            };
-            dom.setUpDropZone(dropZone);
+        for (let card of cards) {
+            card.addEventListener('dragstart', event => {
+                event.dataTransfer.setData("text/plain", card.id);
+            });
+        }
+        ;
+        dom.setUpDropZone(dropZone);
 
     },
-    
+
     // here comes more features
     removeAllBoardElements: function () {
         let allBoards = document.querySelector('#boards');
@@ -403,68 +408,69 @@ export let dom = {
     },
     initColumnTitleRename: function () {
         let columnTitles = document.querySelectorAll('.card-column-title');
-            for (let colTitle of columnTitles) {
-                colTitle.addEventListener("click", function () {
-                    let oldName = `${colTitle.innerText}`;
-                    let columnTitle = `${colTitle.innerText}`;
-                    let addName = `
+        for (let colTitle of columnTitles) {
+            colTitle.addEventListener("click", function () {
+                let oldName = `${colTitle.innerText}`;
+                let columnTitle = `${colTitle.innerText}`;
+                let addName = `
                         <form>
                         <input type="text" name="title" placeholder="${columnTitle}" value="${columnTitle}">
                         <input type="submit" id="new-board-name-submit" value="Save">
                         </form>
                         `
-                    colTitle.insertAdjacentHTML("afterend", addName);
-                    let form = document.querySelector('form')
-                    form.addEventListener('submit', event => {
-                        const formData = new FormData(event.target)
-                        dataHandler.renameColumn(oldName, formData.get('title'));
-                    })
-                });
-            }
+                colTitle.insertAdjacentHTML("afterend", addName);
+                let form = document.querySelector('form')
+                form.addEventListener('submit', event => {
+                    const formData = new FormData(event.target)
+                    dataHandler.renameColumn(oldName, formData.get('title'));
+                })
+            });
+        }
     },
-  setUpDropZone: function (dropZone) {
-          for (let zone of dropZone) {
-              zone.addEventListener('dragover', event => {
-                  event.preventDefault();
-              })
-          };
+    setUpDropZone: function (dropZone) {
+        for (let zone of dropZone) {
+            zone.addEventListener('dragover', event => {
+                event.preventDefault();
+            })
+        }
+        ;
 
-          for (let zone of dropZone) {
-              zone.addEventListener('drop', event => {
-                  let zoneStatus = zone.getAttribute('status');
-                  let droppedElementId = event.dataTransfer.getData("text/plain");
-                  let droppedElement = document.querySelector(`div[id="${droppedElementId}"]`);
-                  zone.appendChild(droppedElement);
-                    if (zoneStatus == 'new') {
-                        droppedElement.setAttribute('class', 'new');
-                        let postData = {'id': droppedElementId, 'status': '0'};
-                        // console.log(postData);
-                        dataHandler._api_post('/update-card', postData, (jsonResponse) => {
+        for (let zone of dropZone) {
+            zone.addEventListener('drop', event => {
+                let zoneStatus = zone.getAttribute('status');
+                let droppedElementId = event.dataTransfer.getData("text/plain");
+                let droppedElement = document.querySelector(`div[id="${droppedElementId}"]`);
+                zone.appendChild(droppedElement);
+                if (zoneStatus == 'new') {
+                    droppedElement.setAttribute('class', 'new');
+                    let postData = {'id': droppedElementId, 'status': '0'};
+                    // console.log(postData);
+                    dataHandler._api_post('/update-card', postData, (jsonResponse) => {
                         dataHandler._data["cards"].push(jsonResponse);
-                        })
-                    } else if (zoneStatus == 'inProgress') {
-                        droppedElement.setAttribute('class', 'inProgress');
-                        let postData = {'id': droppedElementId, 'status': '1'};
-                        // console.log(postData);
-                        dataHandler._api_post('/update-card', postData, (jsonResponse) => {
+                    })
+                } else if (zoneStatus == 'inProgress') {
+                    droppedElement.setAttribute('class', 'inProgress');
+                    let postData = {'id': droppedElementId, 'status': '1'};
+                    // console.log(postData);
+                    dataHandler._api_post('/update-card', postData, (jsonResponse) => {
                         dataHandler._data["cards"].push(jsonResponse);
-                        })
-                    } else if (zoneStatus == 'testing') {
-                        droppedElement.setAttribute('class', 'testing');
-                        let postData = {'id': droppedElementId, 'status': '2'};
-                        // console.log(postData);
-                        dataHandler._api_post('/update-card', postData, (jsonResponse) => {
+                    })
+                } else if (zoneStatus == 'testing') {
+                    droppedElement.setAttribute('class', 'testing');
+                    let postData = {'id': droppedElementId, 'status': '2'};
+                    // console.log(postData);
+                    dataHandler._api_post('/update-card', postData, (jsonResponse) => {
                         dataHandler._data["cards"].push(jsonResponse);
-                        })
-                    } else if (zoneStatus == 'done') {
-                        droppedElement.setAttribute('class', 'done');
-                        let postData = {'id': droppedElementId, 'status': '3'};
-                        // console.log(postData);
-                        dataHandler._api_post('/update-card', postData, (jsonResponse) => {
+                    })
+                } else if (zoneStatus == 'done') {
+                    droppedElement.setAttribute('class', 'done');
+                    let postData = {'id': droppedElementId, 'status': '3'};
+                    // console.log(postData);
+                    dataHandler._api_post('/update-card', postData, (jsonResponse) => {
                         dataHandler._data["cards"].push(jsonResponse);
-                        })
-                    }
-              });
-          }
-      },
+                    })
+                }
+            });
+        }
+    },
 };
